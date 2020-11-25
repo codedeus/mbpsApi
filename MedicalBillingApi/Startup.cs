89091c7cb489a7d6e3c8f7b4d7be57ec4477aa65
nwachukwu <nwachukwu.ugwuanyi@gmail.com>
2020-11-25 13:26:14 +0100

    dotnet core version was dealing with me

--- MedicalBillingApi/Startup.cs
+++ MedicalBillingApi/Startup.cs
@@ -189,7 +189,7 @@ namespace MedicalBillingApi
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("v1/swagger.json", "My API V1");
             });
 
 
